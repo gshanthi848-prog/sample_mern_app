874a3a5a9d8763cf2cae3b96439d344ea025b435
gshanthi848-prog <gshanthi848@gmail.com>
2026-09-21 21:53:06 +0530

    Third Commit

--- routes/emp_route.js
+++ routes/emp_route.js
@@ -1,22 +1,48 @@
-let express=require('express');
+let express=require("express");
 let router=express.Router();
-let {users}=require('../models/users');
+let bcrypt=require('bcrypt');
+let {users} =require('../models/users')
 
-//localhost:3000/api/employes/register
 router.post("/register",async(req,res)=>{
     console.log(req.body);
-    let newUser=new users(req.body);
-    let result=await newUser.save();
+    req.body.password=await bcrypt.hash(req.body.password,10);
+    let newuser=users(req.body);
+    let result=newuser.save();
     res.send(result);
+    // res.send("register route");
+
 });
-router.post("/login",(req,res)=>{
-    console.log(req.body);
-    res.send(req.body);
+router.post("/login",async(req,res)=>{
+    let result=await users.findOne({email:req.body.email})
+    
+    if(result){
+        let matchpass=await bcrypt.compare(req.body.password,result.password);
+         if(matchpass){
+            res.send("login succesfull")
+        }else{
+            res.send("login failed")
+        }
+    }else{
+        res.send("user not found")
+    }
+
 });
-router.get("/viewtask",(req,res)=>{
-    res.send("view task route");
+
+router.get("/viewtasks",(req,res)=>{
+    res.send("view tasks route");
+
 });
-router.put("/updatetask",(req,res)=>{
-    res.send("update task route");
+router.put("/updateStatus",(req,res)=>{
+    res.send("update status  route");
+
+});
+router.patch("/updateprofile/:id",async(req,res)=>{
+  let data=req.body;
+  if(data.password){
+    data.password=await bcrypt.hash(data.password,10);
+  }
+  let result=await users.findByIdAndUpdate(req.params.id,data,{new:true});
+  res.send(result);
+
 });
 module.exports=router;
